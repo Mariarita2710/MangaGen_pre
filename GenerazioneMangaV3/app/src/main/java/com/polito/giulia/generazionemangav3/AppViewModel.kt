d0c853d9fb040123cadbbefc8b0c0b8ca390e6b7
2174 <mariarita.franceschetti@csi.it>
2024-02-12 22:17:01 +0100

    home

--- GenerazioneMangaV3/app/src/main/java/com/polito/giulia/generazionemangav3/AppViewModel.kt
+++ GenerazioneMangaV3/app/src/main/java/com/polito/giulia/generazionemangav3/AppViewModel.kt
@@ -1,11 +1,14 @@
 package com.polito.giulia.generazionemangav3
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,8 +16,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,8 +30,13 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.MutableLiveData
@@ -101,59 +113,72 @@ fun ScrollableColumn(viewModel: AppViewModel) {
     val man by viewModel.mangas.observeAsState()
     println(man)
 
-    Column(
+    val data =
+        listOf<String>(
+            "Attack on Titan",
+            "One punch man",
+            "JoJo",
+            "Attack on Titan",
+            "One punch man",
+            "JoJo","Attack on Titan",
+            "One punch man",
+            "JoJo","Attack on Titan",
+            "One punch man",
+            "JoJo","Attack on Titan",
+            "One punch man",
+            "JoJo",
+        )
+
+
+    LazyColumn(
         modifier = Modifier
-            .fillMaxHeight(0.9f)
-            .padding(10.dp, 74.dp, 10.dp, 10.dp)
-            .verticalScroll(rememberScrollState())
-        ,
+            .padding(top = 80.dp)
+            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        contentPadding = PaddingValues(top = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
-        Spacer(modifier=Modifier.weight(0.25f))
-        Column(modifier = Modifier){
+        items(count = data.count()) { countValue ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+                    .clickable {  },
+                colors = CardDefaults.cardColors(MaterialTheme.colorScheme.secondary)
+            ) {
+                Text(
+                    text = "${data[countValue]}  vol  $countValue",
+                    fontSize = 20.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.ExtraBold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 30.dp , top = 10.dp , end = 10.dp)
+                )
 
-            Row(modifier=Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween){
-                Text(text = "For you", color = MaterialTheme.colorScheme.onPrimary,
-                    fontFamily = fontFamily,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold)
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(10.dp), horizontalArrangement = Arrangement.SpaceEvenly, verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .width(120.dp)
+                            .height(150.dp)
+                            .padding(start = 10.dp , top = 5.dp , bottom = 10.dp),
+                        contentScale = ContentScale.Crop,
+                        painter = painterResource(id = R.drawable.newmanga),
+                        contentDescription = ""
+                    )
+
+                    Text(modifier = Modifier.padding(15.dp),text = "The new ${data[countValue]} volume is out, read it as soon as possible or get spoiled by your friends!" ,color = MaterialTheme.colorScheme.onPrimary,
+                        fontSize = 18.sp,
+                        fontFamily = fontFamily,
+                        textAlign = TextAlign.Start,
+                        fontWeight = FontWeight.Normal )
+                }
             }
-            Spacer(modifier=Modifier.weight(0.25f))
-
-            //Contiene i bottoni con i manga
-            Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
-
-            }
-
         }
-        Spacer(modifier = Modifier.border(2.dp, MaterialTheme.colorScheme.onPrimary))
-    }
-
-    //Seconda riga Consigliati
-    Column(modifier = Modifier){
-
-        Row(modifier=Modifier.fillMaxWidth()){
-            Text(text = "Recommendations", color = MaterialTheme.colorScheme.onPrimary,
-                fontFamily = fontFamily,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold)
-        }
-        Row(modifier = Modifier.horizontalScroll(rememberScrollState())){
-        }
-    }
-    Spacer(modifier = Modifier.border(2.dp, MaterialTheme.colorScheme.onPrimary))
-    //Terza riga
-    Column(modifier = Modifier){
-
-        Row(modifier=Modifier.fillMaxWidth()){
-            Text(text = "Read again", color = MaterialTheme.colorScheme.onPrimary,
-                fontFamily = fontFamily,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold)
-        }
-        Row(modifier = Modifier.horizontalScroll(rememberScrollState())){
-
-        }
-
     }
 }
 @Composable
